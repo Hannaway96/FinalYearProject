c66278b188a87b203bc23739545f8b6e9d2516cf
Jack <jackhans96@gmail.com>
2019-01-29 14:45:07 +0000

    Added Activity Designs
    +WorkoutLog
    +AddExercise

--- app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/WorkoutLog.java
+++ app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/WorkoutLog.java
@@ -2,9 +2,15 @@ package com.example.jack.eee521_finalyearproject_b00708431_fitnesstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class WorkoutLog extends AppCompatActivity {
 
+    Button addExerciseBtn;
+    Button saveWorkoutBtn;
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +18,9 @@ public class WorkoutLog extends AppCompatActivity {
 
         //changes the title of the ActionBar
         getSupportActionBar().setTitle("Workout Log");
+
+        addExerciseBtn = (Button)findViewById(R.id.WorkoutLog_addExerciseBtn);
+        saveWorkoutBtn = (Button)findViewById(R.id.workout_Log_SaveWorkoutBtn);
+        backButton = (ImageButton)findViewById(R.id.workoutLog_backBtn);
     }
 }
