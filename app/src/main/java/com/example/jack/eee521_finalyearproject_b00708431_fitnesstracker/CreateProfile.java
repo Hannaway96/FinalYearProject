8ceae474bc4fe145a045a0284ae0adb55aa3e087
Jack <jackhans96@gmail.com>
2019-01-29 11:36:49 +0000

    Got Add to firestore working can't read

--- app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/CreateProfile.java
+++ app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/CreateProfile.java
@@ -1,27 +1,34 @@
 package com.example.jack.eee521_finalyearproject_b00708431_fitnesstracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateProfile extends AppCompatActivity {
 
     private Button createBtn;
+    private String TAG = "";
     RadioButton maleRadBtn, femaleRadBtn ;
-    //FirebaseDatabase dbUsers = FirebaseDatabase.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,9 @@ public class CreateProfile extends AppCompatActivity {
 
     public void Register_Accepted(View view) throws ParseException {
 
+
+        //TODO date format not working.
+
         String userName = ((EditText)findViewById(R.id.Create_Profile_name_editTxt)).getText().toString();
         double userHeight = Double.parseDouble(((EditText)findViewById(R.id.Create_Profile_height_editTxt)).getText().toString());
         double userWeight = Double.parseDouble(((EditText)findViewById(R.id.Create_Profile_weight_editTxt)).getText().toString());
@@ -47,11 +57,33 @@ public class CreateProfile extends AppCompatActivity {
 
         if(Validation(userName, userHeight, userWeight, userDob, gender) == true){
 
-            //String id = dbUsers.push().getKey();
-            //User user = new User(userName, userHeight, userWeight, userDob, gender);
-            //DatabaseReference dbRef = dbUsers.getReference();
-            //dbRef.setValue(user);
-            //dbUsers.child(id).setValue(user);
+            if(maleRadBtn.isChecked() == true && femaleRadBtn.isChecked() == false){
+                gender = "Male";
+            }
+            else if (femaleRadBtn.isChecked() == true && maleRadBtn.isChecked() == true){
+                gender = "Female";
+            }
+
+            Map<String, Object> userMap =  new HashMap<>();
+            userMap.put("Name", userName);
+            userMap.put("DOB", userDob);
+            userMap.put("Gender", gender);
+            userMap.put("Height(cm)", userHeight);
+            userMap.put("Weight(kg)", userWeight);
+
+            db.collection("Users")
+                    .add(userMap)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "Document added with ID: " + documentReference.getId());
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error adding document");
+                }
+            });
 
             Toast.makeText(CreateProfile.this, "Your profile has be created successfully!", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(CreateProfile.this, StartUp.class);
@@ -68,7 +100,6 @@ public class CreateProfile extends AppCompatActivity {
     public boolean Validation(String username, double height, double weight, Date dob, String gender){
 
         try {
-
             //setting the gender of the user
             if(maleRadBtn.isChecked()==true && femaleRadBtn.isChecked() == false){
                 gender = "Male";
@@ -101,6 +132,7 @@ public class CreateProfile extends AppCompatActivity {
                 Toast.makeText(CreateProfile.this, "Weight must be between 35kg and 200kg", Toast.LENGTH_SHORT).show();
                 return false;
             }
+
 
             else {
                 return true;
--- app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/StartUp.java
+++ app/src/main/java/com/example/jack/eee521_finalyearproject_b00708431_fitnesstracker/StartUp.java
@@ -11,18 +11,24 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 public class StartUp extends AppCompatActivity {
@@ -30,29 +36,32 @@ public class StartUp extends AppCompatActivity {
     final private int REQUEST_INTERNET = 123;
     private Button createProfileBtn;
     private Spinner profileNameSpinner;
+    private List<String> userList = new ArrayList<String>();
     private String TAG = "MyApp";
-
-    DatabaseReference dbUsers;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        FirebaseApp.initializeApp(this);
-        //CheckNetwork checks if the user is connected to the internet
-        CheckNetwork();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
 
         //Changing title of activity
         getSupportActionBar().setTitle("Workout Tracker");
 
+
+        //Checks if the user wishes to close the application
         if(getIntent().getBooleanExtra("EXIT", false)){
             finish();
         }
 
+        //CheckNetwork checks if the user is connected to the internet
+        CheckNetwork();
+
         createProfileBtn = (Button)findViewById(R.id.Start_Up_Create_Profile_btn);
         profileNameSpinner = (Spinner)findViewById(R.id.Start_Up_UserSpinner);
-        //dbUsers = FirebaseDatabase.getInstance().getReference("User/List");
+
+        PopulateSpinner();
     }
 
    public void CheckNetwork(){
@@ -103,6 +112,51 @@ public class StartUp extends AppCompatActivity {
         }
         return false;
     }
+
+
+    public void PopulateSpinner(){
+
+        //TODO Fix PopulateSpinner, currently skips over Query Snapshot as it's not getting the document for some reason.
+
+        int count = 0;
+        String countAsString = Integer.toString(count);
+        final ArrayList<String> users = new ArrayList<>();
+
+        db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if(task.isSuccessful()){
+                    for (DocumentSnapshot document: task.getResult()){
+
+                        User user = document.toObject(User.class);
+                        users.add(user.getUserName());
+                    }
+                }
+            }
+        });
+
+    //   do{
+          //  DocumentReference docRef = db.collection("Users").document(countAsString);
+           // docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+              //  @Override
+              //  public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                //    if(documentSnapshot.exists()){
+             //
+                //    }
+           //     }
+          //  });
+
+
+        //}while(count);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, userList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        profileNameSpinner.setAdapter(dataAdapter);
+    }
+
+
 
     public void CreateProfile(View view){
         Intent intent = new Intent(StartUp.this, CreateProfile.class);
